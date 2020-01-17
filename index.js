f6c2f9ed6d84286e6db78847aef344878928c860
Mees Postma <35431488+MeesPos@users.noreply.github.com>
2020-01-17 11:03:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,76 +1,51 @@
+let arrayMenu = [
+    "Spaghetti met spekjes, spinazie en room",
+    "Macaroni met spekjes, spinazie en room",
+    "Tagliatelli met spekjes, spinazie en room",
+    "Spaghetti met courgette en tomatensaus",
+    "Macaroni met courgette en tomatensaus",
+    "Tagliatelli met courgette en tomatensaus",
+    "Lasagne met courgette en tomatensaus",
+];
 
-let menu = document.getElementById("menukaart");
-let leeg = [];
-let menukaart = [
-    "Spaghetti met ham en kaas",
-    "<br> Macaroni met ham en kaas",
-    "<br> Tagliatelli met ham en kaas",
-    "<br> Spaghetti ppmet spinazie en room",
-    "<br> Macaroni met spinazie en room",
-    "<br> Tagliatelli met spinazie en room",
-    "<br> Spaghetti met gehakt-tomatensaus en kaas",
-    "<br> Macaroni met gehakt-tomatensaus en kaas",
-    "<br> Tagliatelli met gehakt-tomatensaus en kaas",
-    "<br> Spaghetti met spekjes, spinazie en room",
-    "<br> Macaroni met spekjes, spinazie en room",
-    "<br> Tagliatelli met spekjes, spinazie en room",
-    "<br> Spaghetti met courgette en tomatensaus",
-    "<br> Macaroni met courgette en tomatensaus",
-    "<br> Tagliatelli met courgette en tomatensaus",
-    "<br> Lasagne met courgette en tomatensaus",
-    "<br> Lasagne met room, doperwten en tomatensaus",
-    "<br> Lasagne met spinazie en tomatensaus en kaas"];
-menu.innerHTML = menukaart;
-let menukaart1 = menukaart[0];
-let menukaart2 = menukaart[1];
-let menukaart3 = menukaart[2];
-let menukaart4 = menukaart[3];
-let menukaart5 = menukaart[4];
-let menukaart6 = menukaart[5];
-let menukaart7 = menukaart[6];
-let menukaart8 = menukaart[7];
-let menukaart9 = menukaart[8];
-let menukaart10 = menukaart[9];
-let menukaart11 = menukaart[10];
-let menukaart12 = menukaart[11];
-let menukaart13 = menukaart[12];
-let menukaart14 = menukaart[13];
-let menukaart15 = menukaart[14];
-let menukaart16 = menukaart[15];
-let menukaart17 = menukaart[16];
-let menukaart18 = menukaart[17];
+let displayMenuElement = document.getElementById("menu");
+let displayMenuElementSpecific = document.getElementById("menuSpecific");
+let displayMenuElementSpecificReturn = document.getElementById("menuSpecificReturn");
+let displayMenuElementMeerdere = document.getElementById("menuMeerdere");
+let displayMenuText = "";
 
-
-// addNumberToArrayElement(number= 17);
-
-
-
-
-// function addNumberToArrayElement(number) {
-//     for (let count = 0; count < menukaart.length; count++) {
-//         console.log(numbers[i] + number);
-//     }
-// }
-console.log(menukaart.length);
-
-
-showElement(0);
-
-
-showElement(10);
-  function showElement(index){
-
-
-  console.log(menukaart[index]);
-}
-// menukaart.push("pannenkoek met ananas");
-// menukaart.unshift("pannenkoek met banaan");
-
-multipleElement(6, 10);
-
-function multipleElement(van, tot){
-console.log(van);
-console.log(tot);
+function displayMenu() {
+    displayMenuText = "";
+    displayMenuElement.innerHTML = displayMenuText;
+    for(x = 0; x <= arrayMenu.length -1; x++){
+        displayMenuText += (x + 1) + ". " + arrayMenu[x] + "<br>";
+        displayMenuElement.innerHTML = displayMenuText;
+    }
 }
 
-menukaart.reverse();
+function displayMenuSpecific(number) {
+    displayMenuText = "";
+    displayMenuElementSpecific.innerHTML = arrayMenu[number];
+}
+
+function displayMenuSpecificReturn(number) {
+    displayMenuText = "";
+    displayMenuElementSpecificReturn.innerHTML = arrayMenu[number];
+    return arrayMenu[number];
+    stopInNieuw();
+}
+stopInNieuwArray = [];
+
+function stopInNieuw(){
+    arrayMenu.push(arrayMenu[4]);
+    console.log(stopInNieuwArray);
+}
+
+function displayMenuMeerdere(number) {
+    displayMenuText = "";
+    displayMenuElementMeerdere.innerHTML = arrayMenu[number];
+    displayMenuElementMeerdere.innerHTML += arrayMenu[number + 1];
+    displayMenuElementMeerdere.innerHTML += arrayMenu[number + 2];
+    displayMenuElementMeerdere.innerHTML += arrayMenu[number + 3];
+    displayMenuElementMeerdere.innerHTML += arrayMenu[number + 4];
+}
